3172103966bf0b773c5f4ab73c3de0c85bb9e802
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-08 14:56:27 -0300

    buscando repositorios pt2

--- src/pages/Principal/index.js
+++ src/pages/Principal/index.js
@@ -1,6 +1,6 @@
 import { useState, useCallback } from 'react'
 
-import { FaGithub, FaPlus } from 'react-icons/fa'
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa'
 import './principal.css'
 
 import api from '../../services/api'
@@ -10,28 +10,37 @@ export default function Principal() {
     const [newRepo, setNewRepo] = useState('')
     const [repositorios, setRepositorios] = useState([])
 
+    const [buttonLoading, setButtonLoading] = useState(false)
+
     const handleSubmit = useCallback((e) => {
         e.preventDefault();
 
         async function submit() {
-            //pegando os dados que o usuário digitou e consumindo api
-            const response = await api.get(`repos/${newRepo}`)
+            setButtonLoading(true)
+            try {
+                //pegando os dados que o usuário digitou e consumindo api
+                const response = await api.get(`repos/${newRepo}`)
 
-            //pegando os dados devolvidos no get
-            const data = {
-                name: response.data.full_name,
-                created: response.data.created_at,
-                url: response.data.git_url
+                //pegando os dados devolvidos no get
+                const data = {
+                    name: response.data.full_name,
+                    created: response.data.created_at,
+                    url: response.data.git_url
+                }
+
+                //adicionando novo repositório à lista
+                setRepositorios([...repositorios, data])
+                //limpando o input
+                setNewRepo('')
+            }catch(error){
+                console.log(error)
+            }finally{
+                setButtonLoading(false)
             }
 
-            //adicionando novo repositório à lista
-            setRepositorios([...repositorios, data])
-            //limpando o input
-            setNewRepo('')
         }
 
         submit();
-        console.log(repositorios)
 
     }, [newRepo, repositorios]);
 
@@ -51,7 +60,9 @@ export default function Principal() {
                     value={newRepo}
                     onChange={(e) => setNewRepo(e.target.value)}
                 />
-                <button className='submitButton' type='submit'><FaPlus color='#FFF' size={14} /></button>
+                {buttonLoading === true ? (
+                    <button className='submitButton' type='submit'><FaSpinner color='#FFF' size={14} /></button>
+                ) : <button className='submitButton' type='submit'><FaPlus color='#FFF' size={14} /></button>}
             </form>
 
         </div>
