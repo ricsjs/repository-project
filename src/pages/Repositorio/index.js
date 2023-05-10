09b8a2c1743d945b46aad1e2346ff6e53cb4c560
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-10 20:36:19 -0300

    correção de routes

--- src/pages/Repositorio/index.js
+++
@@ -1,8 +0,0 @@
-export default function Repositorio({ match }){
-    return(
-        <div>
-            <h1>teste</h1>
-            
-        </div>
-    )
-}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom'
-import Repositorio from './pages/Repositorio'
 import Principal from './pages/Principal'
 
 export default function RoutesApp(){
@@ -7,7 +6,6 @@ export default function RoutesApp(){
         <Routes>
 
             <Route path='/' element={ <Principal /> } />
-            <Route path='/repositorio/:repositorio' element={ <Repositorio /> }/>
         </Routes>
     )
 }
